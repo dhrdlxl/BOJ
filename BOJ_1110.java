825d6089109967deaff40c0c1df58eb6cb5ed1f4
SeungHyunYoo <dhrdlxl@naver.com>
2020-08-06 16:18:58 +0900

    Math

---
+++ BOJ_1110.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class BOJ_1110 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int target = sc.nextInt();
+        int answer = 0;
+        int num = -1;
+        int copyTarget = target;
+        while(target != num)
+        {
+            answer++;
+            num = (copyTarget / 10 + copyTarget % 10) % 10 + (copyTarget % 10) * 10;
+            copyTarget = num;
+        }
+
+        System.out.println(answer);   
+    }
+}
